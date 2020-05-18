8ec8b773dbc8df39eb2582ab442da6dc85e166a3
zeta-77 <bts.yuichiro@gmail.com>
2020-05-18 20:03:29 +0900

    second correction

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
-n = 1
-while n <= 100
-  if n % 15 == 0
+(1..100).each do |num|
+  if num % 15 == 0
     puts 'FizzBuzz'
-
-  elsif n % 3 == 0
+  elsif num % 3 == 0
     puts 'Fizz'
-
-  elsif n % 5 == 0
+  elsif num % 5 == 0
     puts 'Buzz'
-
   else
-    puts n
-
+    puts num
   end
-  n += 1
 end
